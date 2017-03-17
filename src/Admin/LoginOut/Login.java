58c541d6301f366c9884295843530ff1e2fe6776
Harsh Chaudhry <chaudhry1994harsh@gmail.com>
2017-03-17 19:17:22 +0530

    initial commit

---
+++ src/Admin/LoginOut/Login.java
@@ -0,0 +1,78 @@
+package Admin.LoginOut;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+		Connection con=null;	//connection to the DB is declared 
+		Statement st=null;	//statement to be sent to query the DB is declared
+		ResultSet rs=null;	//resultset to store values returned by the DB is declared
+		try{
+			String email=request.getParameter("email");	//email entered by user on the page is stored in a string
+			String pwd=request.getParameter("pwd");	//password entered by user on the page is stored in a string
+			Class.forName("com.mysql.jdbc.Driver");	//returns com.mysql.jdbc.driver class needed to establish connection with DB
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/seo","root","MySQL");	//connection is initialized, port is given, DB name,password are given
+			st= con.createStatement();	//statement is initialized to be queried with the DB
+			rs=st.executeQuery("select * from admins where email='"+email+"'");	//query is executed and the results are stored in the resultset, which includes the entry which has the email entered by user
+			if(rs.next()){	//if an entry is present in the resultset
+                if (rs.getString(2).equals(pwd)) {	//if the password in the entry is the same as the one entered by user
+                		HttpSession session = request.getSession();	//session for the user will be created that will keep the user logged in
+                		session.setAttribute("admin",rs.getInt(0));	//an attribute for the session is created and is given the value of the ID of the user from the DB
+                		response.sendRedirect("index-1.html");	//user is directed to the next page
+                  }
+                else {
+                    out.println( "Password does not match with the password during registeration... Please re-login with correct password");
+                }
+			}
+			
+		}
+		catch(Exception se)
+        {
+            se.printStackTrace();
+        }
+		finally{
+			try { if (rs != null) rs.close(); } catch (Exception e) {};	//resultset is closed
+			try { if (st != null) st.close(); } catch (Exception e) {};	//statement is closed
+			try { if (con != null) con.close(); } catch (Exception e) {};	//connection is closed
+		}
+	}
+
+}
---
+++ src/Admin/ManageUsers/TerminateA.java
@@ -0,0 +1,73 @@
+package Admin.ManageUsers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class AdminT
+ */
+@WebServlet("/TerminateA")
+public class TerminateA extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TerminateA() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		HttpSession session = request.getSession();	//active sessions are checked
+		if(session.getAttribute("admin")==null){	//if admin session does not exist
+												//link admin login page
+		}
+		else{	//if admin session exists
+			PrintWriter out = response.getWriter();
+			Connection con=null;	//connection to the DB is declared
+			Statement st=null;	//statement to be sent to query the DB is declared
+			try{
+				String email=request.getParameter("email");	//email entered by user on the page is stored in a string
+				Class.forName("com.mysql.jdbc.Driver");	//returns com.mysql.jdbc.driver class needed to establish connection with DB
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/seo","root","MySQL");	//connection is initialized, port is given, DB name,password are given
+				st= con.createStatement();	//statement is initialized to be queried with the DB
+				int i=st.executeUpdate("delete from admins where email = '"+email+"'");	//int i is used to execute statement, deleting the records of the selected admin from the DB
+				if(i>0)	//int i will be greater then 0 if the update to the DB was successful
+				{
+					out.println(" admin terminated.");
+				}
+			}
+			catch(Exception se)
+			{
+				se.printStackTrace();
+			}
+			finally{
+				try { if (st != null) st.close(); } catch (Exception e) {};	//statement is closed
+				try { if (con != null) con.close(); } catch (Exception e) {};	//connection is closed
+			}
+		}
+	}
+
+}
